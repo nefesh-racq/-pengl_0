e0895f22fa0849562fb2ba237b043689a82a994b
ramiro <nefesh_racq_@hotmail.com>
2018-11-20 21:24:17 -0400

    hola mundo en opengl con glut

---
+++ hello_opengl.cpp
@@ -0,0 +1,29 @@
+/**
+* programa basico de opengl con glut
+* g++ hello_opengl.cpp -glut -lGL
+*
+*/
+#include <iostream>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+int main(int argc, char **argv) {
+   glutInit(&argc, argv);
+
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutInitWindowSize(350, 350);
+   glutInitWindowPosition(100, 100);
+   glutCreateWindow("hello opengl and glut");
+
+   glColor3f(1, 0, 0);
+   glBegin(GL_QUADS);
+     glVertex2f( 0.5,  0.5);
+     glVertex2f( 0.5, -0.5);
+     glVertex2f(-0.5, -0.5);
+     glVertex2f(-0.5,  0.5);
+   glEnd();
+
+   glFlush();
+   glutMainLoop();
+   return 0;
+}
